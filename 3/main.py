bfb66bc5e90a0d11be92a6d9082d981d00c342f5
juliadobroszek <dobroszek.julia@gmail.com>
2025-06-17 10:51:28 +0200

    update

--- 3/main.py
+++ 3/main.py
@@ -8,52 +8,41 @@ from sklearn.svm import SVC
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
 file_path = Path(__file__).parent / 'data' / 'bupa.data'
 columns = ['Mcv', 'Alkphos', 'Sgpt', 'Sgot', 'Gammagt', 'Drinks', 'Selector']
 data = pd.read_csv(file_path, header=None, names=columns)
 
-# 2. Przygotowanie danych
 X = data.iloc[:, :-1].values
 y = data.iloc[:, -1].values
-y = np.where(y == 2, 0, 1)  # 1 = chory, 0 = zdrowy
+y = np.where(y == 2, 0, 1)
 
-# 3. Podział na dane treningowe i testowe
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 4. Normalizacja cech
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train) # liczy średnią i odchylenie standardowe
-X_test_scaled = scaler.transform(X_test) # odejmuje od każdej wartości średnią i wynik dzieli na odchylenie standardowe
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-# 5. Optymalizacja parametrów za pomocą GridSearchCV
 param_grid = {
     'C': [0.1, 1, 10, 100],
     'gamma': ['scale', 0.01, 0.1, 1, 10],
     'kernel': ['rbf']
 }
 
-# super vector classifier
-grid = GridSearchCV(SVC(), param_grid, cv=5, scoring='accuracy') # testuje wszystkie kombinacje parametrów
-# cv -> walidacja krzyżowa. Dzieli dane treningowe na 5 i na 4 z nich trenuje 5 jest dla testów
-# scoring -> accuracy oznacza że prównuje modele na podstawie ich dokładności – czyli procentu poprawnych przewidywań
+grid = GridSearchCV(SVC(), param_grid, cv=5, scoring='accuracy')
 grid.fit(X_train_scaled, y_train)
 
 print("Najlepsze parametry:", grid.best_params_)
 print("Najlepszy wynik walidacji:", grid.best_score_)
 
-# 6. Użycie najlepszego modelu do predykcji
 best_model = grid.best_estimator_
 y_pred = best_model.predict(X_test_scaled)
 
-# 7. Wyniki
 print("\nWyniki dla: Najlepszy SVM")
 print(classification_report(y_test, y_pred, digits=3))
 
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Dokładność na zbiorze testowym: {accuracy * 100:.2f}%")
 
-# 8. Macierz pomyłek
 cm = confusion_matrix(y_test, y_pred)
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Zdrowy', 'Chory'], yticklabels=['Zdrowy', 'Chory'])
 plt.title('Macierz pomyłek – SVM (najlepszy)')
@@ -62,7 +51,6 @@ plt.ylabel('Rzeczywistość')
 plt.tight_layout()
 plt.show()
 
-# 9. Wykres błędu w zależności od C
 # C_values = np.logspace(-3, 2, 10)
 # errors = []
 #
